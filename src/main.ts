34a88cf31b04a01711127ea0740f6227c0c90fc0
Sothyra12 <schan256@my.centennialcollege.ca>
2025-05-22 20:58:54 -0400

    worked on toggling the form and localstorage

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import './output.css';
 
-const addTaskBtn = document.getElementById("task-btn") as HTMLButtonElement;
+const addTaskBtn = document.getElementById("open-task-form-btn") as HTMLButtonElement;
 const taskForm = document.getElementById("task-form") as HTMLFormElement;
-const inputTitle = document.getElementById("title") as HTMLInputElement;
+const titleInput = document.getElementById("title") as HTMLInputElement;
 const descField = document.getElementById("description") as HTMLTextAreaElement;
 const statusOpt = document.getElementById("status") as HTMLSelectElement;
 const priorityOpt = document.getElementById("priority") as HTMLSelectElement;
@@ -11,6 +11,42 @@ const todoTaskDiv = document.getElementById("todo-tasks") as HTMLDivElement;
 const inPorgressTaskDiv = document.getElementById("in-progress-tasks") as HTMLDivElement;
 const doneTaskDiv = document.getElementById("done-tasks") as HTMLDivElement;
 
-const tasksData = localStorage.getItem("data") ? JSON.parse(localStorage.getItem("data") as string) : [];
+const taskData = localStorage.getItem("data") ? JSON.parse(localStorage.getItem("data") as string) : [];
 
-let trackCurrentTask = {};
+let trackCurrentTask: any = {};
+
+const removeSpecialChars = (value: string) => {
+    return value.trim().replace(/[^A-Za-z0-9\-\s]/g, "");
+};
+
+const addOrUpdateTask = () => {
+    if (!titleInput.value.trim()) return alert("Please input the task title!");
+
+    const dataArrIndex = taskData.findIndex((item: any) => item.id === trackCurrentTask.id)
+    
+    const taskObj: any = {
+        id: `${removeSpecialChars(titleInput.value)
+            .toLowerCase()
+            .split(" ")
+            .join("-")}-${Date.now()}`,
+        title: removeSpecialChars(titleInput.value),
+        description: removeSpecialChars(descField.value),
+    };
+
+    if (dataArrIndex === -1) {
+        taskData.unshift(taskObj);
+    } else {
+        taskData[dataArrIndex] = taskObj;
+    }
+
+    localStorage.setItem("data", JSON.stringify(taskData)); 
+};
+
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addOrUpdateTask();
+});
+
+addTaskBtn.addEventListener("click", () => {
+    taskForm.classList.toggle("hidden");
+});
